ef9f0e3bdc4667daace379df862e604faa01f186
unknown <bowenwang926>
2023-08-20 05:53:53 -0230

    First Commit

--- main.py
+++ main.py
@@ -3,4 +3,4 @@ import matplotlib.pyplot as plt
 from sklearn.linear_model import LinearRegression
 
 trainingHours = np.array([20, 50, 32, 65, 23, 43, 10, 5, 22, 35, 29, 5, 56])
-parsePercentiles = np.array([[46, 98]])
+parsePercentiles = np.array([56, 98]])
